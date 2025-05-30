9fc38c7c3b75ef28e63fb23a0faac10e23910f9f
CamiPeroto <camilabuenoperoto@gmail.com>
2025-05-30 00:07:06 -0300

    fix: ajustando os meta-dados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,41 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Portifólio | Camila Peroto",
-  description: "Meu Portifólio",
+   title: 'Portifólio | Camila Peroto',
+  description:
+    'Engenheira de software e Desenvolvedora Fullstack  especializada em desenvolvimento de sites e sistemas.',
+  keywords: [
+    'engenharia de software',
+    'desenvolvimento web',
+    'tecnologia',
+    'soluções digitais',
+    'freelancer',
+    'portifólio',
+    'desenvolvedor fullstack',
+    'software personalizado',
+  ],
+  authors: [{ name: 'Camila Bueno Peroto' }], 
+  openGraph: {
+    title: 'Camila Peroto | Desenvolvedora',
+    description:
+      'Engenheira de software e Desenvolvedora Fullstack  especializada em desenvolvimento de sites e sistemas.',
+    siteName: 'Camila Peroto',
+    images: [
+      {
+        url: 'https://github.com/CamiPeroto/portifolio/blob/master/public/images/background.png',
+        width: 1200,
+        height: 630,
+        alt: 'Camila Peroto - Desenvolvedora',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico', 
+    apple: '/apple-touch-icon.png', 
+  }
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
